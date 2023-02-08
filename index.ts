45dd24ed407602584cdce6cfd5452c62399588a8
XPoweR29 <xpower-29@wp.pl>
2023-02-08 17:55:00 +0100

    Added searching and listing ads.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import { handleError, ValidationError } from './utils/errors';
-import rateLimit from 'express-rate-limit';'
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,4 +1,9 @@
 import { AdRecord } from "../records/ad.record"
+import { pool } from "../utils/db";
+
+afterAll(async() => {
+    await pool.end();
+});
 
 test('AdRecord returns data from database for one entry', async () => {
     const ad = await AdRecord.getOne('abc');
@@ -13,4 +18,24 @@ test('AdRecord returns null from DB for unexisting entry', async () => {
     const ad = await AdRecord.getOne('---');
 
     expect(ad).toBeNull();
+})
+
+test('AdRecord.findAll returns array of found entries', async() => {
+    const ads = await AdRecord.findAll('');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+})
+
+test('AdRecord.findAll returns array of found entries when searching "a"', async() => {
+    const ads = await AdRecord.findAll('a');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+})
+
+test('AdRecord.findAll returns empty array when searching for something that does not exist.', async() => {
+    const ads = await AdRecord.findAll('-------------');
+
+    expect(ads).toEqual([]);
 })
